a4e30073d372c76d0afe711cb263bb38d40364b4
g16rim <79007826+g16rim@users.noreply.github.com>
2023-01-10 16:52:03 +0900

    [Silver V] Title: 집합, Time: 4872 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,30 +1,31 @@
 import sys
 
-M = int(sys.stdin.readline())
-mySet = set()
+m = int(input())
+S = set()
 
-for _ in range(M):
-    command = sys.stdin.readline().strip().split()
-
-    if len(command) == 1:
-        if command[0] == 'all':
-            mySet = set([i for i in range(1, 21)])
-        else:
-            mySet = set()
+for _ in range(m):
+    # comm = list(input().split())
+    comm = sys.stdin.readline().strip().split()
+    
+    if len(comm) == 1:
+        if comm[0] == 'all': # all
+            S = set([i for i in range(1, 21)])
+        else: # empty
+            S = set()
     else:
-        func, x = command[0], int(command[1])
+        func, x = comm[0], int(comm[1])
         if func == 'add':
-            mySet.add(x)
+            S.add(x)
         elif func == 'remove':
-            mySet.discard(x) # 삭제할 요소가 없으면 무시
+            S.discard(x) # 삭제할 요소가 없으면 무시
             # remove()는 에러 발생
         elif func == 'check':
-            if x in mySet:
+            if x in S:
                 print(1)
             else:
                 print(0)
         elif func == 'toggle':
-            if x in mySet:
-                mySet.discard(x)
+            if x in S:
+                S.discard(x)
             else:
-                mySet.add(x)
+                S.add(x)
