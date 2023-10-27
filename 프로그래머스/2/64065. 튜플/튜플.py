b93f303faacd75387f2631e9de083f03bf98ad16
Kyungrim Park <79007826+g16rim@users.noreply.github.com>
2023-10-27 20:35:53 +0900

    [level 2] Title: 튜플, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,19 +1,38 @@
+# from collections import Counter
+
+# def solution(s):
+#     elements = [] # 등장하는 모든 원소
+#     answer = []
+#     prev = 0
+#     for char in s:
+#         if char in '{},\"':
+#             if prev != 0:
+#                 elements.append(prev)
+#             prev = 0
+#             continue
+#         prev = prev * 10 + int(char)
+        
+#     for key, value in Counter(elements).most_common():
+#         # 등장 횟수(value) 순으로 내림차순
+#         answer.append(key)
+        
+#     return answer
+
+# 차집합 풀이
 from collections import Counter
 
 def solution(s):
-    elements = [] # 등장하는 모든 원소
     answer = []
-    prev = 0
-    for char in s:
-        if char in '{},\"':
-            if prev != 0:
-                elements.append(prev)
-            prev = 0
-            continue
-        prev = prev * 10 + int(char)
-        
-    for key, value in Counter(elements).most_common():
-        # 등장 횟수(value) 순으로 내림차순
-        answer.append(key)
-        
+    s = s[:-2].replace('{','').replace(',',' ').split('}')
+    
+    elements = [i.split() for i in s]
+    
+    elements.sort(key=lambda x:len(x)) # 길이 오름차순, 등장 횟수 내림차순
+    
+    for tup in elements:
+        minus = Counter(tup) - Counter(answer)
+        answer.append(list(minus.elements())[0])
+    
+    answer = [int(i) for i in answer]
+    
     return answer
