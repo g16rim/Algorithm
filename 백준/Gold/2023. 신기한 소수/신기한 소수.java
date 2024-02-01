9b93cc1c0cca830b6ae52a7f83516acbb9469357
Kyungrim Park <79007826+g16rim@users.noreply.github.com>
2024-02-01 17:22:06 +0900

    [Gold V] Title: 신기한 소수, Time: 76 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	static int N;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		solve(1, 2);
+		solve(1, 3);
+		solve(1, 5);
+		solve(1, 7);
+		
+		System.out.println(sb);
+	}
+	
+	private static void solve(int len, int num) {
+		if (len == N) {
+			sb.append(num).append("\n");
+			return;
+		}
+		
+		if (isPrime(num) == false) return;
+		
+		for (int i = 0; i < 10; i++) {
+			int temp = num * 10 + i;
+			
+			if (isPrime(temp) == true) solve(len + 1, temp); 
+ 		}
+	}
+
+	private static boolean isPrime(int num) {
+		for (int i = 2; i * i <= num; i++) {
+			if (num % i == 0) return false; 
+		}
+		return true;
+	}
+	
+}
