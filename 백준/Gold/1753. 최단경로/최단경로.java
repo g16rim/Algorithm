cafc5487fcaa13268614d8824c0a877f5750be22
Kyungrim Park <79007826+g16rim@users.noreply.github.com>
2024-02-27 15:34:11 +0900

    [Gold IV] Title: 최단경로, Time: 908 ms, Memory: 127444 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,74 +1,79 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class Node {
-		int vertex, weight;
-		Node next;
+	static List<Node>[] graph;
+	static StringBuilder sb = new StringBuilder();
 
-		public Node(int vertex, int weight, Node next) {
-			this.vertex = vertex;
+	static class Node implements Comparable<Node> {
+		int to;
+		int weight;
+		
+		public Node(int to, int weight) {
+			this.to = to;
 			this.weight = weight;
-			this.next = next;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(this.weight, o.weight);
 		}
 	}
-
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int V = Integer.parseInt(st.nextToken());
 		int E = Integer.parseInt(st.nextToken());
 		int start = Integer.parseInt(br.readLine());
-
-		Node[] adjList = new Node[V + 1];
-		int[] minDistance = new int[V + 1];
-		boolean[] visited = new boolean[V + 1];
+		
+		graph = new ArrayList[V + 1];
+		for (int i = 0; i <= V; i++) graph[i] = new ArrayList<>();
 
 		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 			int weight = Integer.parseInt(st.nextToken());
-			adjList[from] = new Node(to, weight, adjList[from]);
+			graph[from].add(new Node(to, weight));
 		}
+		
+		dijkstra(V, start);
+		
+		System.out.println(sb);
+	}
 
+	
+	static void dijkstra(int V, int start) {
+		boolean[] visited = new boolean[V + 1];
+		int[] minDistance = new int[V + 1];
+		
 		Arrays.fill(minDistance, Integer.MAX_VALUE);
 		minDistance[start] = 0;
-
-		int min = 0, stopOver = 0;
-		for (int i = 1; i <= V; i++) { // 모든 정점이 다 처리될 때까지 반복
-			// step1 : 미방문 정점 중 출발지에서 가장 가까운 정점 선택
-			min = Integer.MAX_VALUE;
-			stopOver = -1;
-
-			for (int j = 1; j <= V; j++) {
-				if (!visited[j] && min > minDistance[j]) {
-					min = minDistance[j];
-					stopOver = j;
-				}
-			}
-
-			if (stopOver == -1)
-				break;
-			visited[stopOver] = true;
-
-			// step2 : 미방문 정점들에 대해 선택된 경유지를 거쳐서 가는 비용과 기존 최소 비용을 비교해서 업데이트
-			for (Node temp = adjList[stopOver]; temp != null; temp = temp.next) {
-				if (minDistance[temp.vertex] > min + temp.weight) {
-					minDistance[temp.vertex] = min + temp.weight;
-				}
+		
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(start, 0));
+		
+		while (!pq.isEmpty()) {
+			int temp = pq.poll().to;
+			
+			if (visited[temp]) continue;
+			visited[temp] = true;
+			
+			for (Node next : graph[temp]) {
+				minDistance[next.to] = Math.min(minDistance[next.to], minDistance[temp] + next.weight);
+				pq.add(new Node(next.to, minDistance[next.to]));
 			}
 		}
-		StringBuilder sb = new StringBuilder();
+		
 		for (int i = 1; i <= V; i++) {
-			if (minDistance[i] == Integer.MAX_VALUE) {
-				sb.append("INF\n");
-			} else {
-				sb.append(minDistance[i]).append("\n");
-			}
+			if (minDistance[i] == Integer.MAX_VALUE) sb.append("INF\n");
+			else sb.append(minDistance[i]).append("\n");
 		}
-		System.out.print(sb);
 	}
 }
