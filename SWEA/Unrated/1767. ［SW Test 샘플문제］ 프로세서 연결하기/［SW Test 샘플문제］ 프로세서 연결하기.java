fef292e84c9c28aad7bd1b86b5fcf69ee21fbbe9
Kyungrim Park <79007826+g16rim@users.noreply.github.com>
2024-02-29 12:11:44 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 172 ms, Memory: 37,416 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -46,14 +45,14 @@ public class Solution {
             }
 
             coresDir = new int[cores.size()];
-            setCoresDir(0, 0, 0);
+            setCoresDir(0, 0);
             sb.append("#").append(tc).append(" ").append(result[1]).append("\n");
             cores.clear();
         }
         System.out.print(sb);
     }
 
-    private static int makeLine(int i, int dir) {
+    private static boolean makeLine(int i, int dir) {
         // i번째 core, dir = coresDir[i]
         int x = cores.get(i).x;
         int y = cores.get(i).y;
@@ -78,10 +77,9 @@ public class Solution {
             for (int j = 0; j < history.size(); j++) {
                 originCell[history.get(j).x][history.get(j).y] = 0;
             }
-            return 0;
         }
 
-        return history.size();
+        return canMake;
     }
 
     private static void eraseLine(int i, int dir) {
@@ -98,29 +96,43 @@ public class Solution {
         }
     }
 
+    private static int compareOne() {
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (originCell[i][j] == LINE) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
     // 방향을 고른다
     // 모든 core의 방향을 선택
     // 다 골라지면 임시셀을 만들고(복구용)
     // O(4^12 * N^2)
-    private static void setCoresDir(int idx, int coreCnt, int lineSum) {
+    private static void setCoresDir(int idx, int coreCnt) {
+    	if (coreCnt + (cores.size() - idx) < result[0]) return;
+    	
         if (idx == cores.size()) {
             if (coreCnt > result[0]) {
                 result[0] = coreCnt;
-                result[1] = lineSum;
+                result[1] = compareOne();
             } else if (coreCnt == result[0]) {
-                result[1] = Math.min(result[1], lineSum);
+                result[1] = Math.min(result[1], compareOne());
             }
             return;
         }
         for (int dir = 0; dir < 4; dir++) {
             // 현재 core에서 dir 방향으로 뻗어 나갈거야
             int cnt = coreCnt;
-            int lineCnt = makeLine(idx, dir);
-            if (lineCnt != 0) {
+            boolean canMake = makeLine(idx, dir);
+            if (canMake) {
                 cnt++;
             }
-            setCoresDir(idx + 1, cnt, lineSum + lineCnt);
-            if (lineCnt != 0) {
+            setCoresDir(idx + 1, cnt);
+            if (canMake) {
                 // erase Line
                 eraseLine(idx, dir);
             }
