663dd0118b1b9887caf04b80f206e94bf67732ff
Kyungrim Park <79007826+g16rim@users.noreply.github.com>
2024-03-01 20:22:03 +0900

    [Silver II] Title: 최대 힙, Time: 412 ms, Memory: 29440 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        while (N-- > 0) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (pq.size() == 0) sb.append("0\n");
+                else sb.append(pq.poll()).append("\n");
+            } else pq.add(x);
+        }
+        System.out.print(sb);
+    }
+}
