c5a215eae62992054381e7a335ce0b994df8483c
Kyungrim Park <79007826+g16rim@users.noreply.github.com>
2024-03-03 13:01:29 +0900

    [Silver IV] Title: 수들의 합 2, Time: 120 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int n, m, a[], s[], result;
+    private static int n, m, a[], result;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,19 +12,19 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         a = new int[n + 1];
-        s = new int[n + 1];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             a[i] = Integer.parseInt(st.nextToken());
-            s[i] = s[i - 1] + a[i];
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j <= n; j++) {
-                if (s[j] - s[i] == m) result++;
-            }
+        int i = 0, j = 0, sum = 0;
+        while (j <= n) {
+            if (sum >= m) sum -= a[i++];
+            else if (sum < m) sum += a[j++];
+            if (sum == m) result++;
         }
+
         System.out.println(result);
     }
 }
