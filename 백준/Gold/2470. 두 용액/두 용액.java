557bdd7254e689079ffba3bd5b02103f5c565e6f
Kyungrim Park <79007826+g16rim@users.noreply.github.com>
2024-03-03 18:46:27 +0900

    [Gold V] Title: 두 용액, Time: 284 ms, Memory: 31148 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] liquid = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) liquid[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(liquid);
+
+        int result = Integer.MAX_VALUE;
+        int x = 0, y = 0;
+        int i = 0, j = n - 1;
+        while (i < j) {
+            int diff = liquid[i] + liquid[j];
+            if (Math.abs(diff) < result) {
+                x = liquid[i]; y = liquid[j];
+                result = Math.abs(diff);
+                if (result == 0) break;
+            }
+            if (diff < 0) i++;
+            else if (diff > 0) j--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(x).append(" ").append(y);
+        System.out.println(sb);
+    }
+}
