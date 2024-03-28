dbd34690feb6bef25d8810ab2d0ea9bafd6b8fc1
Kyungrim Park <79007826+g16rim@users.noreply.github.com>
2024-03-28 23:33:18 +0900

    [level 3] Title: 단속카메라, Time: 11.96 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -4,9 +4,7 @@ class Solution {
     public int solution(int[][] routes) {
         int answer = 1;
         
-        Arrays.sort(routes, (a,b) -> {
-            return a[1] - b[1];
-        });
+        Arrays.sort(routes, (a,b) -> a[1] - b[1]);
         
         int pos = routes[0][1];
         for (int i = 1; i < routes.length; i++) {
