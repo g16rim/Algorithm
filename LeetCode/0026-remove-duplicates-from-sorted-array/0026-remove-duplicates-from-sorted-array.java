1acad8da78ada7aeea01db106f5ba7417b39493e
Kyungrim Park <79007826+g16rim@users.noreply.github.com>
2024-05-07 23:24:11 +0900

    Time: 0 ms (100%), Space: 45 MB (5.25%) - LeetHub

--- LeetCode/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ LeetCode/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,12 +1,12 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        int k = 1;
+    public int removeDuplicates(int [] nums) {
+        int k = 1; // 정답 배열 포인터
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] != nums[i + 1]) {
-                nums[k++] = nums[i + 1];
+                nums[k] = nums[i + 1];
+                k++;
             }
         }
-        
         return k;
     }
 }
