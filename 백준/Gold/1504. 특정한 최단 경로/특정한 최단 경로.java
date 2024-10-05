b8d263d34056b339f78fedc88c684ad97407d8d2
Kyungrim Park <79007826+g16rim@users.noreply.github.com>
2024-10-05 21:56:57 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 528 ms, Memory: 68056 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -2,29 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    private static int INF = 200_000_100;
-    private static int n, e, v1, v2, dist[], result1, result2;
-    private static List<Node>[] graph;
-
-    private static class Node implements Comparable<Node> {
+    static int INF = 200_000_000;
+    static int n, e, v1, v2, dist[], result1, result2;
+    static List<Node>[] graph;
+    
+    public static class Node implements Comparable<Node> {
         int end, weight;
-
+        
         Node(int end, int weight) {
             this.end = end;
             this.weight = weight;
         }
-
+        
         @Override
         public int compareTo(Node o) {
             return Integer.compare(this.weight, o.weight);
         }
     }
-
+    
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
         n = Integer.parseInt(st.nextToken());
         e = Integer.parseInt(st.nextToken());
         graph = new ArrayList[n + 1];
@@ -43,32 +42,29 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         v1 = Integer.parseInt(st.nextToken());
         v2 = Integer.parseInt(st.nextToken());
-
-        // 1 -> v1 -> v2 -> n
+        
         result1 += dijkstra(1, v1);
         result1 += dijkstra(v1, v2);
         result1 += dijkstra(v2, n);
-        // 1 -> v2 -> v1 -> n
+        
         result2 += dijkstra(1, v2);
         result2 += dijkstra(v2, v1);
         result2 += dijkstra(v1, n);
-
-        int answer = (result1 >= INF && result2 >= INF) ? -1 : Math.min(result1, result2);
-        System.out.println(answer);
+        
+        System.out.println((result1 >= INF && result2 >= INF) ? -1 : Math.min(result1, result2));
     }
-
-    private static int dijkstra(int s, int e) {
+    
+    static int dijkstra(int s, int e) {
         PriorityQueue<Node> pq = new PriorityQueue<>();
         Arrays.fill(dist, INF);
-        dist[s] = 0;
         pq.add(new Node(s, 0));
-
+        dist[s] = 0;
+        
         while (!pq.isEmpty()) {
             Node cur = pq.poll();
-
-            if (dist[cur.end] < cur.weight)
-                continue;
-
+            
+            if (dist[cur.end] < cur.weight) continue;
+            
             for (Node n : graph[cur.end]) {
                 if (cur.weight + n.weight < dist[n.end]) {
                     dist[n.end] = cur.weight + n.weight;
@@ -76,8 +72,7 @@ public class Main {
                 }
             }
         }
-
+        
         return dist[e];
     }
-
 }
