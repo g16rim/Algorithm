70bf29f5e159db28deae0fd0db96309db8c4d4cb
Kyungrim Park <79007826+g16rim@users.noreply.github.com>
2024-10-29 10:42:03 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 88 ms, Memory: 11916 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,31 +1,29 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int n, arr[], LIS[], max = Integer.MIN_VALUE;
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-
-		n = Integer.parseInt(br.readLine());
-		arr = new int[n];
-		LIS = new int[n];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			LIS[i] = 1;
-		}
-
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < i; j++) {
-				if (arr[j] < arr[i] && LIS[i] < LIS[j] + 1) {
-					LIS[i] = LIS[j] + 1;
-				}
-			}
-			max = Math.max(max, LIS[i]);
-		}
-		System.out.println(max);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+        }
+        
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    
+                }
+            }
+            result = Math.max(result, dp[i]);
+        }
+        
+        System.out.println(result);
+    }
 }
