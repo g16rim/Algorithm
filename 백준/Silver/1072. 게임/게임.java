2eee6aff6cc657e7fd32dc822671d73bac0e51cd
Kyungrim Park <79007826+g16rim@users.noreply.github.com>
2024-11-20 13:15:24 +0900

    [Silver III] Title: 게임, Time: 68 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int z = (int) ((long) y * 100 / x);
+        
+        if (z >= 99) {
+            System.out.println(-1);
+            return;
+        }
+        
+        int start = 1; // 추가로 이긴 게임 수
+        int end = 1_000_000_000;
+        int result = -1;
+        
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            
+            int nx = x + mid;
+            int ny = y + mid;
+            double nz = (int) ((long) ny * 100 / nx);
+            
+            if (z < nz) {
+                result = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        
+        System.out.println(result);
+    }
+}
