1fb1fbd7ae8b136e5deb14c66f05252ed7d596a0
Kyungrim Park <79007826+g16rim@users.noreply.github.com>
2024-12-21 17:02:35 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 64 ms, Memory: 11480 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,39 +1,48 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	static int N, sour[], bitter[], result = 1_000_000_000;
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		sour = new int[N];
-		bitter = new int[N];
-		
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			sour[i] = Integer.parseInt(st.nextToken());
-			bitter[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		generateSubSet(0, 1, 0);
-		
-		System.out.println(result);
-	}
-	
-	private static void generateSubSet(int cnt, int sourFlavor, int bitterFlavor) {
-		if (cnt > 0 && sourFlavor != 1 && bitterFlavor != 0) {
-			result = Math.abs(sourFlavor - bitterFlavor) < result ? Math.abs(sourFlavor - bitterFlavor) : result;			
-		}
-
-		if (cnt == N) {
-			return;
-		}
-		
-		generateSubSet(cnt + 1, sourFlavor * sour[cnt], bitterFlavor + bitter[cnt]);
-		generateSubSet(cnt + 1, sourFlavor, bitterFlavor);
-	}
-
+    static int n;
+    static int[] sour, bitter;
+    static long sourSum, bitterSum, result = 1_000_000_001;
+    static boolean[] visited;
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        sour = new int[n];
+        bitter = new int[n];
+        visited = new boolean[n];
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            sour[i] = Integer.parseInt(st.nextToken());
+            bitter[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        subSet(0, 0);
+        System.out.println(result);
+    }
+    
+    static void subSet(int idx, int cnt) {
+        if (idx == n) {
+            if (cnt > 0) {
+                sourSum = 1;
+                bitterSum = 0;
+                for (int i = 0; i < n; i++) {
+                    if (visited[i]) {
+                        sourSum *= sour[i];
+                        bitterSum += bitter[i];
+                    }
+                }
+                result = Math.min(result, Math.abs(sourSum - bitterSum));
+            }            
+            return;
+        }
+        
+        visited[idx] = true;
+        subSet(idx + 1, cnt + 1);
+        visited[idx] = false;
+        subSet(idx + 1, cnt);
+    }
 }
