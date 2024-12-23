cb85728ea2f3a9f20abadb955dfcffffcf75cd63
Kyungrim Park <79007826+g16rim@users.noreply.github.com>
2024-12-23 14:06:35 +0900

    [Gold V] Title: 신기한 소수, Time: 64 ms, Memory: 11444 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,41 +1,36 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	static int N;
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		
-		solve(1, 2);
-		solve(1, 3);
-		solve(1, 5);
-		solve(1, 7);
-		
-		System.out.println(sb);
-	}
-	
-	private static void solve(int len, int num) {
-		if (len == N) {
-			sb.append(num).append("\n");
-			return;
-		}
-		
-		for (int i = 0; i < 10; i++) {
-			int temp = num * 10 + i;
-			
-			if (isPrime(temp) == true) solve(len + 1, temp); 
- 		}
-	}
-
-	private static boolean isPrime(int num) {
-		for (int i = 2; i * i <= num; i++) {
-			if (num % i == 0) return false; 
-		}
-		return true;
-	}
-	
+    static int n;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        for (int i = 2; i <= 9; i++) {
+            solve(1, i);
+        }
+        System.out.println(sb);
+    }
+    
+    static void solve(int len, int num) {
+        if (!isPrime(num)) return;
+        
+        if (len == n) {
+            sb.append(num).append("\n");
+            return;
+        }
+        
+        for (int i = 1; i <= 9; i += 2) {
+            solve(len + 1, num * 10 + i);
+        }
+    }
+    
+    static boolean isPrime(int n) {
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
 }
