7605b787ac3e51bb892e638fe4746b121d3d14b0
Kyungrim Park <79007826+g16rim@users.noreply.github.com>
2025-03-03 00:12:56 +0900

    [level 2] Title: 타겟 넘버, Time: 127.19 ms, Memory: 174 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 72.9 MB, 시간: 0.41 ms
+메모리: 174 MB, 시간: 127.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 01일 23:38:00
+2025년 03월 03일 00:12:53
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,9 +1,34 @@
+import java.util.*;
+
 class Solution {
     static int answer = 0;
     
     public int solution(int[] numbers, int target) {
-        dfs(numbers, target, 0, 0);
+        // dfs(numbers, target, 0, 0);
+        bfs(numbers, target);
         return answer;
+    }
+    
+    void bfs(int[] numbers, int target) {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[] {numbers[0], 0});
+        q.add(new int[] {-numbers[0], 0});
+        
+        while (!q.isEmpty()) {
+            int[] temp = q.poll();
+            int sum = temp[0];
+            int idx = temp[1];
+            
+            if (idx == numbers.length - 1) {
+                if (target == sum) {
+                    answer++;
+                }
+                continue;
+            }
+            
+            q.add(new int[] {sum + numbers[idx + 1], idx + 1});
+            q.add(new int[] {sum - numbers[idx + 1], idx + 1});
+        }
     }
     
     private void dfs(int[] numbers, int target, int index, int sum) {
