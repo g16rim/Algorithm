b7c318548cea6bcd6691296a562181bec6f143eb
Kyungrim Park <79007826+g16rim@users.noreply.github.com>
2025-03-05 22:52:31 +0900

    [level 3] Title: 가장 먼 노드, Time: 44.42 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,46 +1,44 @@
 import java.util.*;
 
 class Solution {
-    private List<Integer>[] graph;
-    private int[] dist;
-    private int maxCnt;
+    List<Integer>[] graph;
+    boolean[] visited;
+    int[] dist;
+    int maxValue = 0;
     
     public int solution(int n, int[][] edge) {
-        int answer = 0;
         graph = new ArrayList[n + 1];
+        for (int i = 0; i <= n; i++) graph[i] = new ArrayList<>();
+        visited = new boolean[n + 1];
         dist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        for (int i = 1; i <= n; i++)
-            graph[i] = new ArrayList<>();
         for (int i = 0; i < edge.length; i++) {
-            graph[edge[i][0]].add(edge[i][1]);
-            graph[edge[i][1]].add(edge[i][0]);
+            int a = edge[i][0];
+            int b = edge[i][1];
+            graph[a].add(b);
+            graph[b].add(a);
         }
         
-        bfs();
-        // System.out.println(Arrays.toString(dist));
-        for (int i = 1; i <= n; i++) {
-            if (dist[i] == maxCnt) answer++;
-        }
-        return answer;
-    }
-    
-    private void bfs() {
         Queue<int[]> q = new ArrayDeque<>();
         q.add(new int[] {1, 0});
-        dist[1] = 0;
+        visited[1] = true;
+        
         while (!q.isEmpty()) {
-            int[] temp = q.poll();
-            int idx = temp[0];
+            int[] cur = q.poll();
+            maxValue = Math.max(maxValue, cur[1]);
+            dist[cur[0]] = cur[1];
             
-            for (int i = 0; i < graph[idx].size(); i++) {
-                int node = graph[idx].get(i);
-                if (dist[node] > temp[1] + 1) {
-                    dist[node] = temp[1] + 1;
-                    q.add(new int[] {node, temp[1] + 1});
-                    maxCnt = Math.max(maxCnt, temp[1] + 1);
+            for (int next : graph[cur[0]]) {
+                if (!visited[next]) {
+                    q.add(new int[] {next, cur[1] + 1});
+                    visited[next] = true;
                 }
             }
         }
+        
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == maxValue) answer++;
+        }         
+        return answer;
     }
 }
